b9a25cb8b631799aae19c67232359d20ab1f9fcd
codexpawan <shahpawan56@gmail.com>
2024-05-23 13:37:45 +0545

    about page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css'
-import './styles/header.scss'
-import Header  from './components/Header'
-import Footer from './components/Footer'
-import Home from './components/Home'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import "./styles/header.scss";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About_me";
 
 function App() {
-
   return (
-    <div className='flex h-screen px-8 py-4 flex-col justify-between'>
-    <Header />
-    <Home />
-    <Footer />
-    </div>
-  )
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
-
-export default App
+export default App;
